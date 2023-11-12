ea43ae977e711e67a142f620a379bfe4da07ad74
js1247 <jasonshi1247@gmail.com>
2023-11-12 16:25:26 -0500

    4:25

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,7 +1,7 @@
 import '../styles/style.css'
-import javascriptLogo from './javascript.svg'
+/*import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { setupCounter } from '../counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -21,4 +21,5 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('#counter'))*/
+
