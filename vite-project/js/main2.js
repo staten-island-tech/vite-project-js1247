eccccc2fbc428cd0be6d5ca81ebec056edce0bc8
js1247 <jasonshi1247@gmail.com>
2023-11-14 09:21:35 -0500

    zaza

---
+++ vite-project/js/main2.js
@@ -0,0 +1,12 @@
+const Menu = ["Za", "stuff"];
+const Name = "Jason"
+export {Menu, Name};
+// export function setupCounter(element) {
+//   let counter = 0
+//   const setCounter = (count) => {
+//     counter = count
+//     element.innerHTML = `count is ${counter}`
+//   }
+//   element.addEventListener('click', () => setCounter(counter + 1))
+//   setCounter(0)
+// }
