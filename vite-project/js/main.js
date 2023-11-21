13e40543f42ca7baa02562ebdfac6d95a9ae9a29
js1247 <jasonshi1247@gmail.com>
2023-11-20 20:13:10 -0500

    813

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,3 +2,13 @@ import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 import { menu } from "./menu";
 
+document.querySelector("#theme-switch").addEventListener("click", function() {
+    if(document.body.classList.contains("cool")) {
+      document.body.classList.add("warm");
+      document.body.classList.remove("cool");
+    }
+    else {
+      document.body.classList.add("cool");
+      document.body.classList.remove("warm");
+    }
+  });
