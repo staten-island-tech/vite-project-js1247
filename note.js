6fafcbb7defeb0b9a2a571caa933f9cd22e962d4
js1247 <jasonshi1247@gmail.com>
2023-11-30 09:24:54 -0500

    notes

--- note.js
+++ note.js
@@ -1,3 +1,34 @@
 //cd vite-project
 //npm install or i
 //npm run dev
+
+//go get Latvian basketball data
+//show Latvian basketball data
+
+// function greet(name){
+//     const greetPromise = new Promise(function(resolve, reject) {
+//         resolve(`Hello ${name}`);
+//     });
+//     return greetPromise;
+// }
+// const Aaron = greet("Aaron");
+// console.log(Aaron);
+// //handle promise
+// Aaron.then((result)=> {
+//     console.log(result);
+// });
+
+// //REST API
+// const URL = `https://api.quotable.io/random`;
+
+// async function getData(URL){
+//     try {
+//         const response = await fetch (URL);
+//         console.log(response);
+//         const data = await response.json();
+//         console.log(data);
+//         document.querySelector("h1").textContent = data.content;
+//         document.querySelector("h2").textContent = data.author;
+//     } catch (error) {}
+// }
+// getData(URL);
