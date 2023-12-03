2fce26c0a40ad143d014e19cd25aea6172e633e0
js1247 <jasonshi1247@gmail.com>
2023-12-03 11:58:15 -0500

    Update main.js

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -23,99 +23,77 @@ document.querySelector("#theme-switch").addEventListener("click", function () {
     );
   }
 
-//   function insertBurgers() {
-//     const menuFilters = menu.filter((food) => food.type.includes("Burgers"));
-//     menuFilters.forEach((food) => DOMSelectors.cards.insertAdjacentHTML("beforeend",
-//     `<div class="card" id="Burgers">
-//       <h2 class="card-title">${food.name}</h2>
-//       <img src="${food.image}" alt="${food.name}" class="card-image">
-//       <h3 class="card-description">${food.price} Popularity: ${food.popularity}</h3>
-//     </div>
-//     `)
-//     );
-//   }
+  function insertBurgers() {
+    const menuFilters = menu.filter((food) => food.type.includes("Burgers"));
+    menuFilters.forEach((food) => DOMSelectors.cards.insertAdjacentHTML("beforeend",
+    `<div class="card" id="Burgers">
+      <h2 class="card-title">${food.name}</h2>
+      <img src="${food.image}" alt="${food.name}" class="card-image">
+      <h3 class="card-description">${food.price} Popularity: ${food.popularity}</h3>
+    </div>
+    `)
+    );
+  }
 
-//   function insertSides() {
-//     const menuFilters = menu.filter((food) => food.type.includes("Sides"));
-//     menuFilters.forEach((food) => DOMSelectors.cards.insertAdjacentHTML("beforeend",
-//     `<div class="card" id="Sides">
-//       <h2 class="card-title">${food.name}</h2>
-//       <img src="${food.image}" alt="${food.name}" class="card-image">
-//       <h3 class="card-description">${food.price} Popularity: ${food.popularity}</h3>
-//     </div>
-//     `)
-//     );
-//   }
+  function insertSides() {
+    const menuFilters = menu.filter((food) => food.type.includes("Sides"));
+    menuFilters.forEach((food) => DOMSelectors.cards.insertAdjacentHTML("beforeend",
+    `<div class="card" id="Sides">
+      <h2 class="card-title">${food.name}</h2>
+      <img src="${food.image}" alt="${food.name}" class="card-image">
+      <h3 class="card-description">${food.price} Popularity: ${food.popularity}</h3>
+    </div>
+    `)
+    );
+  }
 
-//   function insertDrinks() {
-//     const menuFilters = menu.filter((food) => food.type.includes("Drinks"));
-//     menuFilters.forEach((food) => DOMSelectors.cards.insertAdjacentHTML("beforeend",
-//     `<div class="card" id="Drinks">
-//       <h2 class="card-title">${food.name}</h2>
-//       <img src="${food.image}" alt="${food.name}" class="card-image">
-//       <h3 class="card-description">${food.price} Popularity: ${food.popularity}</h3>
-//     </div>
-//     `)
-//     );
-//   }
-
-//   function insertSpecials() {
-//     const menuFilters = menu.filter((food) => food.type.includes("Specials"));
-//     menuFilters.forEach((food) => DOMSelectors.cards.insertAdjacentHTML("beforeend",
-//     `<div class="card" id="Specials">
-//       <h2 class="card-title">${food.name}</h2>
-//       <img src="${food.image}" alt="${food.name}" class="card-image">
-//       <h3 class="card-description">${food.price} Popularity: ${food.popularity}</h3>
-//     </div>
-//     `)
-//     );
-//   }
-
-DOMSelectors.all.addEventListener("click", function () {
-  DOMSelectors.cards.innerHTML = '';
-  //insertAll();
-
-});
-
-DOMSelectors.burgers.addEventListener("click", function () {
-  DOMSelectors.cards.innerHTML = '';
-  //insertBurgers();
-});
-
-DOMSelectors.sides.addEventListener("click", function () {
-  DOMSelectors.cards.innerHTML = '';
-  //insertSides();
-});
-
-DOMSelectors.drinks.addEventListener("click", function () {
-  DOMSelectors.cards.innerHTML = '';
-  //insertDrinks();
-});
-
-DOMSelectors.specials.addEventListener("click", function () {
-  DOMSelectors.cards.innerHTML = '';
-  //insertSpecials();
-});
-
-insertAll();
-
-
-const buttons = document.querySelectorAll(".food-button");
-buttons.forEach((btn) =>
-  btn.addEventListener("click", function() {
-    let filter = btn.textContent;
-    menu
-      .filter((food) => food.type.includes(filter))
-      .forEach((cards) => foodCreate(cards));
-  })
-);
-
-const foodCreate = function(food) {
-  DOMSelectors.cards.insertAdjacentHTML("beforeend",
+  function insertDrinks() {
+    const menuFilters = menu.filter((food) => food.type.includes("Drinks"));
+    menuFilters.forEach((food) => DOMSelectors.cards.insertAdjacentHTML("beforeend",
     `<div class="card" id="Drinks">
       <h2 class="card-title">${food.name}</h2>
       <img src="${food.image}" alt="${food.name}" class="card-image">
       <h3 class="card-description">${food.price} Popularity: ${food.popularity}</h3>
     </div>
     `)
-};
+    );
+  }
+
+  function insertSpecials() {
+    const menuFilters = menu.filter((food) => food.type.includes("Specials"));
+    menuFilters.forEach((food) => DOMSelectors.cards.insertAdjacentHTML("beforeend",
+    `<div class="card" id="Specials">
+      <h2 class="card-title">${food.name}</h2>
+      <img src="${food.image}" alt="${food.name}" class="card-image">
+      <h3 class="card-description">${food.price} Popularity: ${food.popularity}</h3>
+    </div>
+    `)
+    );
+  }
+
+DOMSelectors.all.addEventListener("click", function () {
+  DOMSelectors.cards.innerHTML = '';
+  insertAll();
+});
+
+DOMSelectors.burgers.addEventListener("click", function () {
+  DOMSelectors.cards.innerHTML = '';
+  insertBurgers();
+});
+
+DOMSelectors.sides.addEventListener("click", function () {
+  DOMSelectors.cards.innerHTML = '';
+  insertSides();
+});
+
+DOMSelectors.drinks.addEventListener("click", function () {
+  DOMSelectors.cards.innerHTML = '';
+  insertDrinks();
+});
+
+DOMSelectors.specials.addEventListener("click", function () {
+  DOMSelectors.cards.innerHTML = '';
+  insertSpecials();
+});
+
+insertAll();
